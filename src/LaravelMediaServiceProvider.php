8052f9c365779e8200e621b0597cc8d495d85411
moh3nmusavi <moh3n.m2014@gmail.com>
2024-03-30 01:38:06 +0330

    add composer option and create "LaravelMediaServiceProvider"

---
+++ src/LaravelMediaServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Moh3n\LaravelMedia;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMediaServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
