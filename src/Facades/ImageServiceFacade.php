d0a25b70dd4b87fa4728cdbc1fc7ffc218672b1e
moh3nmusavi <moh3n.m2014@gmail.com>
2024-04-06 01:11:25 +0330

    fix method static upload in ImageServiceFacade

--- src/Facades/ImageServiceFacade.php
+++ src/Facades/ImageServiceFacade.php
@@ -1,9 +1,13 @@
 <?php
 namespace Moh3n\LaravelMedia\Facades;
+use Illuminate\Http\UploadedFile;
 use \Illuminate\Support\Facades\Facade ;
 
 class ImageServiceFacade extends Facade
 {
+    /**
+     * @method static upload(UploadedFile $file , bool $isPrivate = false)
+     */
     protected static function getFacadeAccessor()
     {
         return 'image-service';
