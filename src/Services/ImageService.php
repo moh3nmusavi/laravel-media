3fac8160dd6eb66d419120d56bf0d6d12ef0cf27
moh3nmusavi <moh3n.m2014@gmail.com>
2024-04-05 17:40:09 +0330

    add `upload` function in ImageService

--- src/Services/ImageService.php
+++ src/Services/ImageService.php
@@ -1,6 +1,17 @@
 <?php
-
+use \Illuminate\Http\UploadedFile ;
 class ImageService
 {
+    /**
+     * upload images
+     * @param UploadedFile $file
+     * @param string $filename
+     * @param string $dir
+     * @return void
+     */
+    public function upload(UploadedFile $file , string $filename , string $dir)
+    {
+        //TODO 
+    }
 
 }
